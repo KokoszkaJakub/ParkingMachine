06fd60049d5d48afc21dd85212ff2bfa683e36b4
Kuba <Kuba@JK-Komputer>
2019-06-17 23:26:44 +0200

    nadal nie dziala

--- ParkingMachine/src/machine/Mthird.java
+++ ParkingMachine/src/machine/Mthird.java
@@ -5,12 +5,7 @@ import javax.swing.JOptionPane;
 public class Mthird {
 	public static void main(String[] args) {
 
-		/*
-		 * while (kwota < doZaplaty) { monetaTxt =
-		 * JOptionPane.showInputDialog("Podaj nominał"); kwota +=
-		 * Integer.parseInt(monetaTxt);
-		 */
-		int cost = 5;
+		int cost = 4;
 
 		String coinTxt = JOptionPane.showInputDialog("Podaj nominał");
 		int coinValue = Integer.parseInt(coinTxt);
@@ -19,8 +14,8 @@ public class Mthird {
 			coinTxt = JOptionPane.showInputDialog("Podaj nominał");
 			coinValue += Integer.parseInt(coinTxt);
 		}
-		JOptionPane.showMessageDialog(null, "Dziękujemy za skorzystanie z naszych usług. " + "Kwota wrzucona: " + coinValue
-				+ ", reszta: " + (coinValue - cost));
+		JOptionPane.showMessageDialog(null, "Dziękujemy za skorzystanie z naszych usług. " + "Kwota wrzucona: "
+				+ coinValue + ", reszta: " + (coinValue - cost));
 
 	}
 
