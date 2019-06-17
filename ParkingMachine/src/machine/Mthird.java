2a0cb3259ac988a62086f90c4c0c87520e15c5b3
Kuba <Kuba@JK-Komputer>
2019-06-17 23:39:46 +0200

    dodanie sprawdzenia poprawnosci wartosci monet 1,2,5

--- ParkingMachine/src/machine/Mthird.java
+++ ParkingMachine/src/machine/Mthird.java
@@ -3,20 +3,31 @@ package machine;
 import javax.swing.JOptionPane;
 
 public class Mthird {
+
+	public static boolean IfCoin(int moneta) {
+		if (moneta == 1 || moneta == 2 || moneta == 5) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
 	public static void main(String[] args) {
+		int cost = 5;
+		int sumCoin = 0;
+		while (sumCoin < cost) {
+			String coinTxt = JOptionPane.showInputDialog("Podaj nominał");
+			int coin = Integer.parseInt(coinTxt);
+			if (IfCoin(coin)) {
+				sumCoin += coin;
+			} else {
+				JOptionPane.showMessageDialog(null, "Nieprawidłowy nominał");
+			}
 
-		int cost = 4;
-
-		String coinTxt = JOptionPane.showInputDialog("Podaj nominał");
-		int coinValue = Integer.parseInt(coinTxt);
-
-		while (coinValue < cost) {
-			coinTxt = JOptionPane.showInputDialog("Podaj nominał");
-			coinValue += Integer.parseInt(coinTxt);
 		}
 		JOptionPane.showMessageDialog(null, "Dziękujemy za skorzystanie z naszych usług. " + "Kwota wrzucona: "
-				+ coinValue + ", reszta: " + (coinValue - cost));
-
+				+ sumCoin + "zł" + ", reszta: " + (sumCoin - cost) + "zł");
 	}
 
 }
